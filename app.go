48aad8a427db78111b7b165959abb09d6970185e
Stephen Lane-Walsh <slanewalsh@mywebgrocer.com>
2015-08-04 18:03:47 -0400

    Updated to work with new goingup changes Updated app.go to work with changes to goingup Removed login/register templates

--- app.go
+++ app.go
@@ -6,15 +6,16 @@ func main() {
 	app := goingup.NewApp()
 	app.Options.Port = 8080
 
-	app.AddPage("/", "Home", "page", "home")
-	app.AddPage("/resume", "Resume", "page", "resume")
-	app.AddPage("/projects", "Projects", "page", "projects")
+	app.AddPage(goingup.NewPage("/", "Home", "page", "home"))
+	app.AddPage(goingup.NewPage("/", "Home", "page", "home"))
+	app.AddPage(goingup.NewPage("/resume", "Resume", "page", "resume"))
+	app.AddPage(goingup.NewPage("/projects", "Projects", "page", "projects"))
 
-	app.Options.Menus["Main"] = []goingup.MenuItem{
-		{URL: "/", Text: "Home"},
-		{URL: "/resume", Text: "Resume"},
-		{URL: "/projects", Text: "Projects"},
-	}
+	mainMenu := goingup.NewMenu();
+	mainMenu.AddItem(goingup.NewMenuItem("/", "Home"))
+	mainMenu.AddItem(goingup.NewMenuItem("/resume", "Resume"))
+	mainMenu.AddItem(goingup.NewMenuItem("/projects", "Projects"))
+	app.AddMenu("main", mainMenu)
 
 	app.Run()
 }
